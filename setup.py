f8e9705b6591044e60e22ff995bd283f7e9c2304
Moamal Hussin <125461788+devmoamal@users.noreply.github.com>
2023-02-15 15:48:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
+import os
+import subprocess
 from setuptools import setup, find_packages
+
+genit_version = (
+    subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
+    .stdout.decode("utf-8")
+    .strip()
+)
+
+if "-" in genit_version:
+    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
+    # pip has gotten strict with version numbers
+    # so change it to: "1.3.3+22.git.gdf81228"
+    # See: https://peps.python.org/pep-0440/#local-version-segments
+    v,i,s = genit_version.split("-")
+    genit_version = v + "+" + i + ".git." + s
+
+assert "-" not in genit_version
+assert "." in genit_version
+
+assert os.path.isfile("genit/version.py")
+with open("genit/VERSION", "w", encoding="utf-8") as fh:
+    fh.write("%s\n" % genit_version)
 
 classifiers  = [
     'Development Status :: 5 - Production/Stable',
@@ -10,14 +33,17 @@ classifiers  = [
 
 setup(
     name='genit',
-    version='0.0.1',
+    version=genit_version,
     description='Simple integer and string Generator',
-    Long_description=open ("README.txt").read() + "\n\n" + open('CHANGELOG.txt').read(),
+    Long_description= open("README.md").read() + "\n\n" + open('CHANGELOG.txt').read(),
     url = "", 
     author='Moamal Hussin',
     author_email='dev.moamal@gmail.com',
     License= 'MIT',
     classifiers=classifiers,
+    python_requires=">=3.5",
+    entry_points={"console_scripts": ["gentit = gentit.main:main"]},
+    package_data={"genit": ["VERSION"]},
     keywordse='',
     packages=find_packages(), 
     install_requires=['']
