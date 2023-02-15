9a7f12b514d3a9078696c1a2d2a5e9a6600e9ee6
Moamal Hussin <125461788+devmoamal@users.noreply.github.com>
2023-02-15 15:53:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,10 +9,6 @@ genit_version = (
 )
 
 if "-" in genit_version:
-    # when not on tag, git describe outputs: "1.3.3-22-gdf81228"
-    # pip has gotten strict with version numbers
-    # so change it to: "1.3.3+22.git.gdf81228"
-    # See: https://peps.python.org/pep-0440/#local-version-segments
     v,i,s = genit_version.split("-")
     genit_version = v + "+" + i + ".git." + s
 
@@ -36,7 +32,7 @@ setup(
     version=genit_version,
     description='Simple integer and string Generator',
     Long_description= open("README.md").read() + "\n\n" + open('CHANGELOG.txt').read(),
-    url = "", 
+    url = "https://github.com/devmoamal/genit", 
     author='Moamal Hussin',
     author_email='dev.moamal@gmail.com',
     License= 'MIT',
